da2cadca35c1953cb5e113a694cf653d4df5013a
Josue Sanchez Nima <josuesancheznima@gmail.com>
2024-04-19 11:38:36 -0500

    Re

--- src/semana12/Semana12.java
+++ src/semana12/Semana12.java
@@ -1,21 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package semana12;
 
-/**
- *
- * @author LAB G
- */
+import semana12.Config.Conexion;
+
 public class Semana12 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Conexion conexion = new Conexion();
-        System.out.println(conexion.conexion());
-    }
-    
+  public static void main(String[] args) {
+    Conexion conexion = new Conexion();
+    System.out.println(conexion.conexion());
+  }
+
 }
